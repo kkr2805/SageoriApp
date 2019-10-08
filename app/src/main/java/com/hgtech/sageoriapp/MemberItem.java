5962ed82530df398f04e771d886a85fdb1a5f1e4
erich0929 <erich0929@naver.com>
2019-10-09 02:52:43 +0900

    JSON 응답을 MemeberItems 와 Mapping.

--- app/src/main/java/com/hgtech/sageoriapp/MemberItem.java
+++ app/src/main/java/com/hgtech/sageoriapp/MemberItem.java
@@ -1,7 +1,18 @@
 package com.hgtech.sageoriapp;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class MemberItem {
+    @SerializedName("MEMBER_ID")
+    @Expose
     public int No;
+
+    @SerializedName("NAME")
+    @Expose
     public String Name;
+
+    @SerializedName("HP")
+    @Expose
     public String HP;
 }
